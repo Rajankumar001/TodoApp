dfbaff82db2b94e889b2168e3eca2df5412080cc
Rajankumar001 <chaudharyrajan387@gmail.com>
2024-03-20 21:01:59 +0530

    all data updated

--- TodoApp/src/App.tsx
+++ TodoApp/src/App.tsx
@@ -9,26 +9,30 @@ function App() {
   interface TodoItem {
     _id: string;
     toDo: string;
+    setUpdateUI: React.Dispatch<React.SetStateAction<boolean>>;
   }
  
   const [todos, setTodos] = useState<TodoItem[]>([]);
   const [input, setInput] = useState<string>("");
+  const [updateUI,setUpdateUI]=useState(false);
 
   useEffect(() => {
     axios.get(`${baseURL}/home`)
       .then((res) => {
         console.log("Received todos:", res.data);
-        setTodos(res.data); 
+        setTodos(res.data.data);
+        setUpdateUI(prevState => !prevState); 
       })
       .catch((err) => {
         console.log("Error caught:", err);
       });
-  }, []);
+  }, [updateUI]);
 
   const saveTodo = () => {
     axios.post(`${baseURL}/save`, { toDo: input })
       .then((res) => {
         console.log("Todo saved:", res.data);
+        setUpdateUI((prevState)=>!prevState)
        setInput("");
       })
       .catch((err) => {
@@ -50,9 +54,8 @@ function App() {
         <button onClick={saveTodo}>Add</button>
       </div>
       <div className="Todolist">
-        <Todolist />
-        <Todolist />
-        <Todolist />
+       {Array.isArray(todos) && todos.map((el)=><Todolist key={el._id} text={el.toDo} id={el._id} setUpdateUI={setUpdateUI}/>)} 
+        
       </div>
       </div>
      
--- TodoApp/src/components/Todolist.tsx
+++ TodoApp/src/components/Todolist.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import { CiEdit } from "react-icons/ci";
 import { MdOutlineDangerous } from "react-icons/md";
-import './Todolist.css'
-
-const  Todolist = () => {
+import './Todolist.css';
+import { baseURL } from '../utils/constant';
+import axios from 'axios';
+interface TodoProps {
+  id:string,
+  text: string; // Specify the type of 'text' as string
+  setUpdateUI: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const  Todolist: React.FC<TodoProps> = ({ text,id,setUpdateUI}) => {
+  const Tododelete=()=>{
+    axios.delete(`${baseURL}/delete/${id}` )
+        .then((res) => {
+          console.log("Todo saved:", res.data);
+          setUpdateUI((prevState)=>!prevState)
+        })
+        .catch((err) => {
+          console.error("Error while saving todo:", err);
+        });
+  }
   return (
     <div className='list'>
-      todo
+    {text}
       <div className="icons">
         <CiEdit className='icon'/>
-        <MdOutlineDangerous className='icon'/>
+        <MdOutlineDangerous className='icon' onClick={Tododelete}/>
       </div>
     </div>
   )
